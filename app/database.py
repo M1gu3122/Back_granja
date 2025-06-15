697b725df9c0ff1f5d096a994001fa662845f82f
M1gu3122 <153031660+M1gu3122@users.noreply.github.com>
2025-06-15 14:36:49 -0500

    Update database.py

--- app/database.py
+++ app/database.py
@@ -3,10 +3,10 @@ import pymysql.cursors
 
 def get_db_connection():
     return pymysql.connect(
-        host="localhost",
-        user="root",
-        password="",
-        database="granja",
-        # port=54667,
+        host="bwn3szb1llqmpbpcjwho-mysql.services.clever-cloud.com",
+        user="unxbcce7majnekgx",
+        password="jgFBRuK72ic2mXEubGk4",
+        database="bwn3szb1llqmpbpcjwho",
+        port=3306,
         cursorclass=pymysql.cursors.DictCursor,
     )
