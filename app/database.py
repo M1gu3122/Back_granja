201e0126b8cd1f4826ab8a902876e9589f4cc9ca
M1gu3122 <153031660+M1gu3122@users.noreply.github.com>
2025-09-17 21:55:04 -0500

    Update database.py

--- app/database.py
+++ app/database.py
@@ -3,10 +3,12 @@ import pymysql.cursors
 
 def get_db_connection():
     return pymysql.connect(
-        host="hopper.proxy.rlwy.net",
+        # mysql://root:MeUqMeSQfSRgleonPpcZOPCtHUyTGfiR@mainline.proxy.rlwy.net:30108/railway
+        host="mainline.proxy.rlwy.net",
         user="root",
-        password="ttjIMaodEIILskmUKpuXVpbCBFiwcJTZ",
+        password="MeUqMeSQfSRgleonPpcZOPCtHUyTGfiR",
         database="railway",
-        port=56046,
+        port=30108,
         cursorclass=pymysql.cursors.DictCursor,
     )
+
